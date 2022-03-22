5afe657f34aa41b6a6da9849e01a8e342e888638
Joe Fong <joe.fong@madetech.com>
2022-03-22 17:48:01 +0000

    initial commit

---
+++ Models/Note.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace reactnet.Models
+{
+    public class Note
+    {
+        public int id { get; set; }
+
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+
+    }
+}
